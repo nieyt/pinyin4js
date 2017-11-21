233e24d08e1119be8750cd5c73e22ad8b43b4f92
nieyt <1123082855@qq.com>
2017-11-21 14:15:24 +0800

    增加排列组合

--- pinyin4js.js
+++ pinyin4js.js
@@ -1,41 +1,103 @@
-/*******************************************************************************
- * Pinyin4JS - Translator of Chinese characters to Pinyin for Internet
- * Copyright 2014 xiuhuwang.com
- *
- * @author xiuhuwang <xiuhuwang@gmail.com>
- * @website http://www.xiuhuwang.com/
- * @version 0.0.7 (2014-02-13)
- *******************************************************************************/
-var pinyin4js =
-{
+import unicodeToPinyinJson from './unicode_to_pinyin4.js';
+
+class Translate{
+    constructor(){
+    }
+
+    /**
+     * @param {string} name 
+     */
+    go(name){
+        var arrs;
+        arrs = this.ToUnicodeWithoutTitle(name);
+        if (!arrs) return; //如果传入的name不是中文
+        arrs = this.ToPinYin(arrs);
+        arrs = this.removeTone(arrs);
+        arrs = this.permutation(arrs);
+        return arrs;
+    }
+
+    /**
+     * 去除音调，再去重
+     * @param {array} arrs 
+     */
+    removeTone(arrs){
+        return arrs.map(item => {
+            item = item.join('').split(/\d/);
+            item.pop();
+            return Array.from(new Set(item));
+        })
+    }
+
+    /**
+     * 排列组合
+     * @param {array} arrs 
+     */
+    permutation(arrs){
+        var arrL = arrs.length;
+        var group = [];
+        /**
+         * 循环体
+         * @param {array} item 
+         * @param {number} i 
+         * @param {string} str 
+         * @return {array} {{所有可能的排列}}
+         */
+        var loop = (item, i, str) => {
+            var arr;
+            if (!str) {
+                arr = [];
+            } else {
+                arr = str.split(',')
+            }
+            for(var k = 0, l = item.length; k < l; k++){
+                if (!str) {
+                    arr = [];
+                } else {
+                    arr = str.split(',')
+                }   //重置arr 避免push多个值
+                arr.push(item[k]);
+                var temp = arr.join(',');
+                var t = i;
+                if(i == arrL-1)
+                    group.push(arr.join(','));
+                if(i < arrL-1)
+                    loop(arrs[++t], t, temp);
+            }
+        };
+        loop(arrs[0], 0);
+
+        return group.map(item => item.split(',').join(''));
+    }
+
+    /**
+     *根据传入的汉字的Unicode编码数组，查找拼音
+     * @param Array
+     */
+    ToPinYin(array){
+        var pinyinArray = new Array();
+        for (var index in array) {
+            pinyinArray.push(this.findPinyinByUnicode(array[index])); 
+        }
+        return pinyinArray;
+    }
 
     /**
      * 把汉字转换为Unicode字符，初始版本使用了escape()函数，后续修改
      * @param str
      * @returns {string}
-     * @constructor
      */
-    ToUnicode: function (str) {
+    ToUnicode(str){
         return escape(str).replace(/%/g, "\\").toUpperCase();
-    },
+    }
 
-    UnUnicode: function (str) {
+
+    UnUnicode(str){
         return unescape(str.replace(/\\/g, "%"));
-    },
+    }
 
-    copyingTxt: function (str) {
-        document.getElementById(str).select();
-        document.execCommand("Copy");
-    },
-
-    /**
-     * 去除汉字Unicode编码的前两位"\u"
-     * @param str
-     * @returns {string}
-     * @constructor
-     */
-    ToUnicodeWithoutTitle: function (str) {
-		alert(1);
+    ToUnicodeWithoutTitle(str){
+        if (!new RegExp("^[\\u4e00-\\u9fa5]+$").test(str)) return false;
         var unicodeArray = this.ToUnicode(str)//.split("\\u"); //取消自动分组
         var unicodeWithoutTitleArray = new Array();
         var i = 0;
@@ -43,31 +105,16 @@ var pinyin4js =
             unicodeWithoutTitleArray.push(unicodeArray.substring(i + 2, i + 6));
             i += 6;
         }
-        return unicodeWithoutTitleArray.toString();
-    },
-
-    /**
-     *根据传入的汉字的Unicode编码数组，查找拼音
-     * @param Array
-     * @constructor
-     */
-    ToPinYin: function (array) {
-        var pinyinArray = new Array();
-        for (var index in array) {
-            pinyinArray.push(this.findPinyinByUnicode(array[index]));
-        }
-        return pinyinArray;
-
-    },
+        return unicodeWithoutTitleArray;
+    }
 
     /**
      * 根据Unicode查找拼音，由于多音字的存在，返回数组
      * @param str
      * @returns {*|unicodeToPinyinJson.pinyin}
      */
-    findPinyinByUnicode: function (str) {
-        
-        for (var unicodeToPinyinObject in unicodeToPinyinJson) {
+    findPinyinByUnicode(str){
+        for (var unicodeToPinyinObject of unicodeToPinyinJson) {
             if (str == unicodeToPinyinObject.unicode) {
                 return unicodeToPinyinObject.pinyin;
             }
@@ -75,3 +122,5 @@ var pinyin4js =
     }
 
 }
+
+export default new Translate();
